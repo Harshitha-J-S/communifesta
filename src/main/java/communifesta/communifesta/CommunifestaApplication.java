2fa4be65c4309cc9284b056478e46bbb4f655dfe
Harshitha-JS <harshithajs04@gmail.com>
2024-02-05 22:04:56 +0530

    Initial commit by adding all dependencies

---
+++ src/main/java/communifesta/communifesta/CommunifestaApplication.java
@@ -0,0 +1,13 @@
+package communifesta.communifesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunifestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunifestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/communifesta/communifesta/CommunifestaApplicationTests.java
@@ -0,0 +1,13 @@
+package communifesta.communifesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunifestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
